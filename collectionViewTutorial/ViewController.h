59b7c86a1691a9c8d4d7bb7af4e4d1401e03c0ff
admin <admin@admins-iMac.local>
2017-04-27 11:30:50 -0700

    Initial Commit

---
+++ collectionViewTutorial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  collectionViewTutorial
+//
+//  Created by admin on 4/27/17.
+//  Copyright © 2017 dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ collectionViewTutorial/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  collectionViewTutorial
+//
+//  Created by admin on 4/27/17.
+//  Copyright © 2017 dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
